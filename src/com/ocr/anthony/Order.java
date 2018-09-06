b587b70917cc2c606f17289ed22fa941e000941c
laenzo <amdiatoutimera@hotmail.fr>
2018-09-06 23:50:51 +0200

    new method added in Order - method runMenu
    method main updated

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -28,5 +30,15 @@ public class Order {
         }else {
             System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
         }
+    }
+
+    /**
+     * Run asking process for a menu.
+     */
+    public void runMenu(){
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb);
     }
 }
